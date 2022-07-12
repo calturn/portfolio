b07f106e569047551a9fddabbce4685b9ecce445
bi0nical <63443600+bi0nical@users.noreply.github.com>
2022-07-12 17:05:42 +1200

    added more styling, images and JavaScript

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
+window.onload = function resetStates() {
+    document.getElementById("pg1").classList.add("active-pg");
+}
+
 function scroller(){ 
     const projects = document.getElementById("projects");
     var windowHeight = window.innerHeight;
@@ -6,10 +10,46 @@ function scroller(){
     if (elementTop < windowHeight - elementVisible) {
         console.log("we here");
         document.getElementById("pg1").style.backgroundColor = "transparent";
-        document.getElementById("pg2").style.backgroundColor = "#FAF8EB";
+        document.getElementById("pg2").classList.add("active-pg");
     } else {
         document.getElementById("pg1").style.backgroundColor = "#FAF8EB";
-        document.getElementById("pg2").style.backgroundColor = "transparent";
+        document.getElementById("pg2").classList.remove("active-pg");
     }
 };
 window.addEventListener("scroll", scroller);
+
+// Kiwistay hide & show
+
+document.getElementById("kiwistay").addEventListener("mouseover", (event) => {
+    document.getElementById("kiwistayImg").classList.add("show");
+    document.getElementById("idleImg").classList.add("hide");
+});
+
+document.getElementById("kiwistay").addEventListener("mouseout", (event) => {
+    document.getElementById("kiwistayImg").classList.remove("show");
+    document.getElementById("idleImg").classList.remove("hide");
+});
+
+// meanwhile hide & show
+
+document.getElementById("meanwhile").addEventListener("mouseover", (event) => {
+    document.getElementById("meanwhileImg").classList.add("show");
+    document.getElementById("idleImg").classList.add("hide");
+});
+
+document.getElementById("meanwhile").addEventListener("mouseout", (event) => {
+    document.getElementById("meanwhileImg").classList.remove("show");
+    document.getElementById("idleImg").classList.remove("hide");
+});
+
+// preloved hide & show
+
+document.getElementById("preloved").addEventListener("mouseover", (event) => {
+    document.getElementById("prelovedImg").classList.add("show");
+    document.getElementById("idleImg").classList.add("hide");
+});
+
+document.getElementById("preloved").addEventListener("mouseout", (event) => {
+    document.getElementById("prelovedImg").classList.remove("show");
+    document.getElementById("idleImg").classList.remove("hide");
+});
